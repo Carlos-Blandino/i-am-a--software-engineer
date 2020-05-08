df4ed82622be5079356166d8ae418bde64a7f1ab
Carlos Blandino <cablandino66@gmail.com>
2020-05-08 13:02:55 -0700

    Initial Commit

---
+++ i-am-a-software-engineer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i-am-a-software-engineer
+//
+//  Created by Carlos Blandino on 5/8/20.
+//  Copyright © 2020 Carlos Blandino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
